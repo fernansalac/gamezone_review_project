8a8c9e6a3515818d10881bf466faf06907c8a998
Fernan Salac <salacfernan@gmail.com>
2020-05-16 20:03:31 +0800

    custom fonts lesson 17

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+//import to create/ use the font
+import * as Font from 'expo-font';
 import Home from './screens/home';
+import { AppLoading } from 'expo';
+
+//function that returns a registered fonts
+const getFonts = () => Font.loadAsync({
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+});
+
 
 export default function App() {
-  return (
-    <Home />
-  );
+  //check if its loaded
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    )
+  }
+
 }
 
