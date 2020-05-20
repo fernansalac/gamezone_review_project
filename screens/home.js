a5cf1794fe1a74bc609bf3205c484b800d83fd81
Fernan Salac <salacfernan@gmail.com>
2020-05-20 21:33:04 +0800

    lesson 22, passing data between screens

--- screens/home.js
+++ screens/home.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
+import ReviewDetails from './reviewDetails';
 
 export default function Home({ navigation }) {
 
-    const pressHandler= () => {
-        navigation.navigate('ReviewDetails')
-        //or
-        //navigation.push('ReviewDetails')
-    }
+    const [reviews, setReviews] = useState([
+        {title: 'Zelda Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1'},
+        {title: 'Gotta Catch them all (again)', rating: 4, body: 'lorem ipsum', key: '2'},
+        {title: 'Not so "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3'}
+    ]);
 
     return(
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
-            <Button title='go to review details' onPress={pressHandler} />
+            <FlatList  
+                data={reviews}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{ item.title }</Text>
+                    </TouchableOpacity>
+                )}
+
+            />
         </View>
     )
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function ReviewDetails({ navigation }) {
@@ -10,8 +10,9 @@ export default function ReviewDetails({ navigation }) {
 
     return(
         <View style={globalStyles.container}>
-            <Text>ReviewDetails Screen</Text>
-            <Button title='back to Home screen' onPress={pressHandler} />
+            <Text>{navigation.getParam('title')}</Text>
+            <Text>{navigation.getParam('body')}</Text>
+            <Text>{navigation.getParam('rating')}</Text>
         </View>
     )
 }
