a9ae473986f38429862b00ddbc66a6ddeff2a800
Fernan Salac <salacfernan@gmail.com>
2020-05-22 20:57:44 +0800

    lesson 27 using images

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/cards';
 
 export default function ReviewDetails({ navigation }) {
+    const rating = navigation.getParam('rating');
 
     const pressHandler = () => {
         navigation.goBack();
@@ -14,8 +15,22 @@ export default function ReviewDetails({ navigation }) {
             <Card>
                 <Text>{navigation.getParam('title')}</Text>
                 <Text>{navigation.getParam('body')}</Text>
-                <Text>{navigation.getParam('rating')}</Text>
+                <View style={styles.rating}>
+                    <Text>GameZone rating: </Text>
+                    <Image source={images.ratings[rating]} />
+                </View>
             </Card>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee'
+    }
+})
