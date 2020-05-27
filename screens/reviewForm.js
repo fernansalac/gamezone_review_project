70040dc2dd4262e9d10c57196754ef90bf706f5f
Fernan Salac <salacfernan@gmail.com>
2020-05-27 22:42:09 +0800

    lessong 34 custom button, end of the tutorial

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { globalStyles } from '../styles/global.js';
 import { Formik } from 'formik';
 import * as yup from 'yup';
+import FlatButton from '../shared/button';
 
 //validation with yup
 const reviewSchema = yup.object({
@@ -17,7 +18,13 @@ const reviewSchema = yup.object({
     rating: yup.string()
     .test('is-num-1-5', 'Rating must be a number 1 - 5', (val) => {
         return parseInt(val) < 6 && parseInt(val) > 0;
-    })
+    }),
+
+    // email: yup.string()
+    // .test('email-validation', 'Enter a valid email', (val) => {
+    //   return  (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val));
+    // })
+
 })
 
 
@@ -26,7 +33,7 @@ export default function ReviewForm({addReview}) {
     return (
         <View style={globalStyles.container}>
             <Formik
-                initialValues={{ title: '', body: '', rating: '' }}
+                initialValues={{ title: '', body: '', rating: '', email: '' }}
                 //validation with yup
                 validationSchema={reviewSchema}
                 onSubmit={(values, actions) => {
@@ -46,7 +53,7 @@ export default function ReviewForm({addReview}) {
                         <Text style={globalStyles.errorText}>{ formikProps.touched.title && formikProps.errors.title }</Text>
 
                         <TextInput
-                            multiline
+                            multiline minHeight={60}
                             style={globalStyles.input}
                             placeholder='Review body'
                             onChangeText={formikProps.handleChange('body')}
@@ -65,7 +72,17 @@ export default function ReviewForm({addReview}) {
                         />
                         <Text style={globalStyles.errorText}>{ formikProps.touched.rating && formikProps.errors.rating }</Text>
 
-                        <Button title='Submit' color='maroon' onPress={formikProps.handleSubmit} />
+                        {/* <TextInput
+                            style={globalStyles.input}
+                            placeholder='Youremail@domain.com'
+                            onChangeText={formikProps.handleChange('email')}
+                            value={formikProps.values.email}
+                            keyboardType='email-address'
+                            onBlur={formikProps.handleBlur('email')}
+                        />
+                        <Text style={globalStyles.errorText}>{ formikProps.touched.email && formikProps.errors.email }</Text> */}
+
+                        <FlatButton text='submit' onPress={formikProps.handleSubmit} />
                     </View>
                 )}
             </Formik>
