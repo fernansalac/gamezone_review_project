81de460b539d50f0600fbcbaa471f0b29b3a82e9
Fernan Salac <salacfernan@gmail.com>
2020-05-28 19:33:49 +0800

    additional 1 test axios

--- screens/about.js
+++ screens/about.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { globalStyles } from '../styles/global';
+import HeadQuarters from './headQuarters';
 
 export default function About() {
     return(
         <View style={globalStyles.container}>
             <Text>About Screen</Text>
+            <HeadQuarters />
         </View>
     )
 }
--- screens/headQuarters.js
+++ screens/headQuarters.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/cards';
+import axios from 'axios';
 
 export default function HeadQuarters() {
-
-
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        alert('a');
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+            console.log(res);
+            setUsers(res.data.slice(0, 100));
+        })
+        
+      }, []);
     return (
+        
         <View style={globalStyles.container}>
             <Card>
                 <Text>Cristo Rey</Text>
             </Card>
+            
+            <FlatList
+                data={users}
+                renderItem={({ item }) => (
+                    //passing data between screens
+                        <Card>
+                            <Text style={globalStyles.titleText}>{item.name}</Text>
+                        </Card>
+                )}
+
+            />
         </View>
     )
 }
